ac7aca69993cf008895126596a0ad1eb0daef8bd
M Natoli <natoli.missy@gmail.com>
2021-07-13 20:48:13 -0500

    added pwa feature

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -66,7 +66,8 @@ const handlePizzaSubmit = event => {
   })
   .catch(err => {
     console.log(err);
-  })
+    saveRecord(formData);
+  });
 };
 
 $pizzaForm.addEventListener('submit', handlePizzaSubmit);
